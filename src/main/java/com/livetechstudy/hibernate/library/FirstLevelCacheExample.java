8aa7f6845996b1b34f766c4c99bfbbb39d4dffb8
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2019-04-25 17:16:45 +0530

    First Level Cache

---
+++ src/main/java/com/livetechstudy/hibernate/library/FirstLevelCacheExample.java
@@ -0,0 +1,37 @@
+package com.livetechstudy.hibernate.library;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.livetechstudy.hibernate.library.dao.HibernateSessionFactory;
+import com.livetechstudy.hibernate.library.entity.Book;
+
+public class FirstLevelCacheExample {
+
+	public static void main(String[] args) {
+		Session firstSession = HibernateSessionFactory.getSessionFactory().openSession();
+		Session secondSession = HibernateSessionFactory.getSessionFactory().openSession();
+		Transaction firstTransaction = null;
+		Transaction secondTransaction = null;
+		try {
+			firstTransaction= firstSession.beginTransaction();
+			secondTransaction= secondSession.beginTransaction();
+			
+			Book book1 = firstSession.load(Book.class, "1234");		
+			System.out.println(book1);
+			Book book2 = firstSession.load(Book.class, "1234");
+			System.out.println(book2);
+			
+			Book book3 = secondSession.load(Book.class, "1234");
+			System.out.println(book3);
+			
+		} finally {
+			firstTransaction.commit();
+			secondTransaction.commit();
+			firstSession.close();
+			secondSession.close();
+			HibernateSessionFactory.shutdown();
+		}
+	}
+
+}
